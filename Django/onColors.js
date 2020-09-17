c76a486176529f52fce23a063c45572d59630f2a
Akhilesh Rai <arsrocz@gmail.com>
2020-09-17 15:18:09 +0530

    Create onColors.js

---
+++ Django/onColors.js
@@ -0,0 +1,8 @@
+function changeDivOne(){
+    document.getElementById('divOne').style.backgroundColor="green";
+}
+
+function changeDivTwo(){
+    document.getElementById('divTwo').style.backgroundColor="orange";
+}
+
