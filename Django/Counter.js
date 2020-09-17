b4e9962eb6b249bee0522084c45fef8f717c4b4c
Akhilesh Rai <arsrocz@gmail.com>
2020-09-17 20:01:31 +0530

    Create Counter.js

---
+++ Django/Counter.js
@@ -0,0 +1,11 @@
+var myVar=0;
+
+function myFunction(){
+    myVar+=1;
+    document.getElementById("para").innerHTML=myVar;
+}
+
+function myFunction2(){
+    myVar-=1;
+    document.getElementById("para").innerHTML=myVar;
+}
