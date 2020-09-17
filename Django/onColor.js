83fba502be9e5edee8b6c340fee7ee056770c460
Akhilesh Rai <arsrocz@gmail.com>
2020-09-17 11:32:12 +0530

    Create onColor.js

---
+++ Django/onColor.js
@@ -0,0 +1,5 @@
+function myFunction(){
+    document.getElementById("para").innerHTML="New Text";
+    document.getElementById("para").style.backgroundColor="blue";
+    document.getElementById("para").style.border="2px solid red";
+}
