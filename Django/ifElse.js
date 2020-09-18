03a33c891f387ab6e126a309f58c98664a172b5e
Akhilesh Rai <arsrocz@gmail.com>
2020-09-18 19:53:17 +0530

    Create ifElse.js

---
+++ Django/ifElse.js
@@ -0,0 +1,10 @@
+
+function run(){
+    var checker = 323;
+    var value = 345;
+    if(checker==value){
+        document.getElementById("output").innerHTML="Equal";
+    } else{
+        document.getElementById("output").innerHTML="Not Equal";
+    }
+}
