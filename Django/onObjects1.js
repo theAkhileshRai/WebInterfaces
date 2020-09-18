37fa4f5dabd79ba6073efe7046d63d11cb7ec73c
Akhilesh Rai <arsrocz@gmail.com>
2020-09-18 19:33:04 +0530

    Create onObjects1.js

---
+++ Django/onObjects1.js
@@ -0,0 +1,7 @@
+var myCarObject={colour:"red",maxSpeed:"200km/h",seats:2,wheels:4};
+var myObject = {name:"John",eyeColor:"Blue",skill:"teaching"};
+var countryPopulations = {england:34000,france:435254,india:3243243242};
+function run(){
+    var value = myCarObject.colour;
+    document.getElementById("output").innerHTML=value;
+}
